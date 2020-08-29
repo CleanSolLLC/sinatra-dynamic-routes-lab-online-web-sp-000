534e51ebf19fcf61e8cbd14bb48ffd6b7da3d295
Mark May <mamark90@gmail.com>
2020-08-29 22:30:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
     
   get 'say/:number/:phrase' do
     
-     "#{params[:phrase]}" * 
+     "#{params[:phrase]}" * params[:number].to_i
   end
   
   get '/:operation/:number1/:number2' do
